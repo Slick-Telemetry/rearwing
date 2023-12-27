e85762b935774b865b980dbeb31d4f1716e11c3b
Pratik Borole <borolepratik@gmail.com>
2023-12-27 23:46:30 +0530

    added /schedule/{year} path

---
+++ constants.py
@@ -0,0 +1,15 @@
+DEFAULT_TESTING_YEAR: int = 2023
+
+EVENT_SCHEDULE_DATETIME_DTYPE_LIST = [
+    "EventDate",
+    "Session1Date",
+    "Session1DateUtc",
+    "Session2Date",
+    "Session2DateUtc",
+    "Session3Date",
+    "Session3DateUtc",
+    "Session4Date",
+    "Session4DateUtc",
+    "Session5Date",
+    "Session5DateUtc",
+]
---
+++ utils.py
@@ -0,0 +1,5 @@
+from constants import DEFAULT_TESTING_YEAR
+
+
+def get_default_year() -> int:
+    return DEFAULT_TESTING_YEAR
