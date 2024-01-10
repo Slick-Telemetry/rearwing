cadd365d4a8e28a173dd0c36ba30e292dd65ae47
Pratik Borole <borolepratik@gmail.com>
2024-01-10 21:15:18 +0530

    updated app port to 8081 for avoiding collision with frontend-cypress

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import uvicorn
 
 def start_server():
     uvicorn.run(
-        "app.main:app", host="127.0.0.1", port=8080, reload=True, log_level="info"
+        "app.main:app", host="127.0.0.1", port=8081, reload=True, log_level="info"
     )
 
 
