8c42d97ad0f9520094ce2e54d38ec8c26ba3085f
Pratik Borole <borolepratik@gmail.com>
2024-01-23 12:45:30 +0530

    ignored app/test_main.py from uvicorn reload

--- run.py
+++ run.py
@@ -3,7 +3,12 @@ import uvicorn
 
 def start_server():
     uvicorn.run(
-        "app.main:app", host="127.0.0.1", port=8081, reload=True, log_level="info"
+        "app.main:app",
+        host="127.0.0.1",
+        port=8081,
+        reload=True,
+        log_level="info",
+        reload_excludes=["app/test_main.py"],
     )
 
 
